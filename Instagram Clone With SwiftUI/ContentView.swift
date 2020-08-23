59289bb8ff4a3862fb03ea89312f592546b0ec22
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-08-23 10:43:38 +0530

    Instagram With SwiftUI - Instagram Design SwiftUI

--- Instagram Clone With SwiftUI/ContentView.swift
+++ Instagram Clone With SwiftUI/ContentView.swift
@@ -7,15 +7,70 @@
 //
 
 import SwiftUI
+import Firebase
+
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, World!")
+        TabView {
+            NavigationView {
+                
+                Home().navigationBarTitle("Instagram")
+                    .navigationBarItems(leading: Button(action: {
+                        
+                    }, label: {
+                        Image("cam").resizable().frame(width: 30, height: 30)
+                    })
+                        .foregroundColor(Color("darkAndWhite"))
+                        , trailing:
+                        
+                        HStack {
+                            Button(action: {
+                                
+                            }) {
+                               Image("IGTV").resizable().frame(width: 30, height: 30)
+                            }.foregroundColor(Color("darkAndWhite"))
+                            
+                            Button(action: {
+                                
+                            }) {
+                               Image("send").resizable().frame(width: 30, height: 30)
+                            }.foregroundColor(Color("darkAndWhite"))
+                            
+                    })
+            }.tabItem {
+                Image("home")
+            }
+            
+            Text("Find").tabItem {
+                Image("find")
+            }
+            
+            Text("Upload").tabItem {
+                Image("plus")
+            }
+            
+            Text("Likes").tabItem {
+                Image("heart")
+            }
+            
+            Text("Profile").tabItem {
+                Image("people")
+            }
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Home: View {
+    var body: some View {
+        
+         Text("Home")
+        
     }
 }
