e73ac804230cda39eea3704cb507359a26b69179
Mahesh Prasad <mahesh223prasad@gmail.com>
2020-08-23 08:30:23 +0530

    Initial Commit

---
+++ Instagram Clone With SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Instagram Clone With SwiftUI
+//
+//  Created by Mahesh Prasad on 23/08/20.
+//  Copyright © 2020 CreatesApp. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
